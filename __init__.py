848c2407005e7e745d4682c24f466d65a4cab0a1
Bhavani Ravi <bhava0895@gmail.com>
2018-06-15 21:37:43 +0530

    integrating main script with Df class

--- __init__.py
+++ __init__.py
@@ -4,50 +4,31 @@ import os
 import yaml
 import copy
 
+from dialogflow import Intent
+
 MODEL_DIRECTORY = './projects/default/'
 SAMPLE_CONFIG = "sample_configs/config_spacy.yml"
 
 
-def construct_intents(training_data):
-    """
-    Constructs Core intents from NLU training data
-    :param training_data:
-    :return: list of intent names
-    """
-    examples = training_data["rasa_nlu_data"]["common_examples"]
-    return list(set([eg["intent"] for eg in examples]))
-
-
-def construct_templates(df_directory):
+def construct_templates(intents):
     """
     Constructs templates from Dialogflow training data
-    :param training_data:
+    :param intents:
     :return: Core-intents file
     """
-    intent_directory = df_directory+"/intents/"
-    final_list = []
-    for file in os.listdir(intent_directory):
-        with open(intent_directory+file,"r") as f:
-            intent_file = json.load(f)
-            try:
-                responses = intent_file["responses"][0]
-                texts = responses["messages"][0]["speech"]
-                action = responses["action"]
-                if type(texts) != type([]):
-                    texts = [texts]
-                final_list.append({"action":action, "text":texts })
-
-                ## Slot filling prompts
-                for entity in responses["parameters"]:
-                    if entity["required"]:
-                        action = action + "_without_" + entity["name"]
-                        texts = [prompt["value"] for prompt in entity["prompts"]]
-                        final_list.append({"action": action, "text": texts})
-            except TypeError as e:
-                print (e)
-                # Ignore non-intent files"
-                continue
-    return final_list
+    template_list = []
+    action_list = []
+    for intent in intents:
+        texts = intent.responses.messages
+        template_list.append({"action":intent.action, "text":texts })
+        action_list.append(intent.action)
+        ## Slot filling prompts
+        for entity in intent.entities:
+            if entity.required:
+                action = intent.action + "_without_" + entity.name
+                action_list.append(action)
+                template_list.append({"action": action, "text": entity.prompts})
+    return action, template_list
 
 
 def write_domain_file(intents, actions, templates):
@@ -55,16 +36,16 @@ def write_domain_file(intents, actions, templates):
         yaml.dump({"intents":intents, "actions":actions, "templates": templates}, outfile, default_flow_style=False)
 
 
-def construct_domain(df_directory, training_data):
+def construct_domain(intents, training_data):
     """
     Construct core domain
+    :param intents,
     :param training_data:
     :return:
     """
-    intents = construct_intents(training_data)
-    templates = construct_templates(df_directory)
-    actions = copy.deepcopy(intents)
-    write_domain_file(intents, actions, templates)
+    intent_list = [intent.name for intent in intents]
+    actions, templates = construct_templates(intents)
+    write_domain_file(intent_list, actions, templates)
 
 
 def construct_interpreter(training_data):
@@ -77,37 +58,32 @@ def construct_interpreter(training_data):
         outfile.write(training_data.as_markdown())
 
 
-def construct_stories_md_string(intents):
+def construct_stories_md_string(intent):
     final_string = u""
     try:
-        if type(intents) == type([]):
-            return ""
-        intent = intents["name"]
-        story_title = intent
-        responses = intents["responses"][0]
-        action = responses["action"]
+        action = intent.action
         entity_dict = {}
 
-        ## handling slots
-        slot_list = [{context["name"]:context["name"]} for context in responses["affectedContexts"]]
+        # handling slots
+        slot_list = [{context.name:context.name} for context in intent.context_out]
         slot_str = "".join(["slot"+str(slot)+"\n" for slot in slot_list])
         slot_str = "-"+slot_str if slot_str else ""
-        ## entity required case
-        for entity in responses["parameters"]:
-            entity_dict[entity["name"]] = entity["value"]
-            if entity["required"]:
-                entity_name = entity["name"]
-                final_string += f"\n## {story_title}_without_{entity_name}\n    * {intent}\n        - {action}_without_{entity_name}\n    {slot_str} "
+
+        # entity required case
+        for entity in intent.entities:
+            entity_dict[entity.name] = entity.value
+            if entity.required:
+                final_string += f"\n## {intent.name}_without_{entity.name}\n    * {intent}\n        - {action}_without_{entity.name}\n    {slot_str} "
 
         entity_dict = str(entity_dict) if entity_dict else ""
 
-        return final_string + f"\n## {story_title}\n    * {intent}{entity_dict} \n         - {action}\n    {slot_str}"
+        return final_string + f"\n## {intent.name}\n    * {intent}{entity_dict} \n         - {action}\n    {slot_str}"
     except (TypeError) as e:
         print (e)
         # Ignore non-intent files"
         return ""
 
-def construct_stories(df_directory):
+def construct_stories(intents):
     """
     Constructs Core actions from NLU training data
     :param training_data:
@@ -115,15 +91,24 @@ def construct_stories(df_directory):
     """
 
     md_string = u""
-    intent_directory = df_directory + "/intents/"
-    for file in os.listdir(intent_directory):
-        with open(intent_directory + file, "r") as f:
-            intent_file = json.load(f)
-            md_string += construct_stories_md_string(intent_file)
-
+    for intent in intents:
+        md_string += construct_stories_md_string(intent)
     with open('stories.md', 'w') as outfile:
         outfile.write(md_string)
 
+
+def construct_df_intents_to_objects(df_directory):
+    intent_directory = df_directory + "/intents/"
+    intents = []
+    for file in os.listdir(intent_directory):
+        if file.endswith("_usersays_en.json"):
+            continue
+        with open(intent_directory + file, "r") as f:
+            intent_file = json.load(f)
+            intent = Intent(intent_file)
+            intents.append(intent)
+    print (intents)
+    return intents
 
 
 def construct_rasa_core(df_directory):
@@ -131,13 +116,15 @@ def construct_rasa_core(df_directory):
     #step --1
     training_data = load_data(df_directory)
     dict_training_data = json.loads(training_data.as_json())
-    construct_domain(df_directory, dict_training_data)
-
-    # Step -2
     construct_interpreter(training_data)
 
+    # Step -2
+    intents = construct_df_intents_to_objects(df_directory)
+    construct_domain(intents, dict_training_data)
+
+
     # #step - 3
-    construct_stories(df_directory)
+    construct_stories(intents)
 
 
 construct_rasa_core("/home/bhavani/apps/mlandds/rasa_nlu/data/examples/WordBot")
--- dialogflow.py
+++ dialogflow.py
@@ -3,7 +3,7 @@ class Intent:
         self.name = intent_json["name"]
         intent_json = intent_json["responses"][0]
         self.entities = [Entity(params) for params in intent_json["parameters"]]
-        self.out_context = [OutContext(context) for context in intent_json["affectedContexts"]]
+        self.context_out = [OutContext(context) for context in intent_json["affectedContexts"]]
         self.action = intent_json["action"]
         self.responses = Responses(intent_json["messages"])
         # self.user_says = UserSays(self.name)
@@ -17,6 +17,7 @@ class Entity:
         self.name = entity_json["name"]
         self.required = entity_json["required"]
         self.prompts =[prompt["value"] for prompt in entity_json["prompts"]]
+        self.value = entity_json["value"]
 
 
 class OutContext:

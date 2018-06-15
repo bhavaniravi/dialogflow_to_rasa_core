01d3a88eed51a909d5ef79f49161a160e45b7044
Bhavani Ravi <bhava0895@gmail.com>
2018-06-15 21:19:20 +0530

    fix json issues

--- dialogflow.py
+++ dialogflow.py
@@ -1,8 +1,9 @@
 class Intent:
     def __init__(self, intent_json):
         self.name = intent_json["name"]
-        self.entites = Entity(intent_json["parameters"])
-        self.input_context = Context(intent_json["affectedContexts"])
+        intent_json = intent_json["responses"][0]
+        self.entities = [Entity(params) for params in intent_json["parameters"]]
+        self.out_context = [OutContext(context) for context in intent_json["affectedContexts"]]
         self.action = intent_json["action"]
         self.responses = Responses(intent_json["messages"])
         # self.user_says = UserSays(self.name)
@@ -15,17 +16,14 @@ class Entity:
     def __init__(self, entity_json):
         self.name = entity_json["name"]
         self.required = entity_json["required"]
-        self.prompts =[ prompt["value"] for prompt in entity_json["prompts"]]
-        return self
+        self.prompts =[prompt["value"] for prompt in entity_json["prompts"]]
 
 
-class Context:
+class OutContext:
     def __init__(self, context_json):
         self.name = context_json["name"]
-        return self
 
 
 class Responses:
     def __init__(self, response_json):
-        self.messages = [prompt["speech"] for prompt in response_json["messages"]]
-        return self
+        self.messages = [prompt["speech"] for prompt in response_json]

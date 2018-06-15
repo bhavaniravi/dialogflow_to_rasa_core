aceafb3227299b02799047e45ed778e4d4564b7d
Bhavani Ravi <bhava0895@gmail.com>
2018-06-15 16:27:14 +0530

    add script to construct domain & training

---
+++ __init__.py
@@ -0,0 +1,113 @@
+from rasa_nlu.training_data import load_data
+from rasa_nlu.training_data.formats.dialogflow import DialogflowReader
+import json
+import os
+import yaml
+import copy
+
+MODEL_DIRECTORY = './projects/default/'
+SAMPLE_CONFIG = "sample_configs/config_spacy.yml"
+DATA_PATH = 'data/examples/rasa/demo-rasa.json'
+
+
+def construct_intents(training_data):
+    """
+    Constructs Core intents from NLU training data
+    :param training_data:
+    :return: Core-intents file
+    """
+    examples = training_data["rasa_nlu_data"]["common_examples"]
+    return list(set([eg["intent"] for eg in examples]))
+
+
+def construct_templates(df_directory):
+    """
+    Constructs templates from NLU training data
+    :param training_data:
+    :return: Core-intents file
+    """
+    intent_directory = df_directory+"/intents/"
+    final_list = []
+    for file in os.listdir(intent_directory):
+        with open(intent_directory+file,"r") as f:
+            intent_file = json.load(f)
+            try:
+                responses = intent_file["responses"][0]
+                texts = responses["messages"][0]["speech"]
+                if type(texts) != type([]):
+                    texts = [texts]
+                final_list.append({"action":responses["action"],
+                            "text":texts })
+            except TypeError:
+                # Ignore non-intent files"
+                continue
+    return final_list
+
+
+def write_domain_file(intents, actions, templates):
+    with open('domain.yml', 'w') as outfile:
+        yaml.dump({"intents":intents, "actions":actions, "templates":templates}, outfile, default_flow_style=False)
+
+
+def construct_domain(df_directory, training_data):
+    """
+    Construct core domain
+    :param training_data:
+    :return:
+    """
+    intents = construct_intents(training_data)
+    templates = construct_templates(df_directory)
+    actions = copy.deepcopy(intents)
+    write_domain_file(intents, actions, templates)
+
+
+def construct_interpreter(training_data):
+    """
+    Constructs Core actions from NLU training data
+    :param training_data:
+    :return: Core-intents file
+    """
+    with open('nlu_data.md', 'w') as outfile:
+        outfile.write(training_data.as_markdown())
+
+
+def construct_stories(df_directory):
+    """
+    Constructs Core actions from NLU training data
+    :param training_data:
+    :return: Core-intents file
+    """
+    intent_directory = df_directory + "/intents/"
+    final_list = []
+    for file in os.listdir(intent_directory):
+        with open(intent_directory + file, "r") as f:
+            intent_file = json.load(f)
+            try:
+                responses = intent_file["responses"][0]
+                texts = responses["messages"][0]["speech"]
+                if type(texts) != type([]):
+                    texts = [texts]
+                final_list.append({"action": responses["action"],
+                                   "text": texts})
+            except TypeError:
+                # Ignore non-intent files"
+                continue
+    return final_list
+
+
+
+def construct_rasa_core(df_directory):
+
+    # step --1
+    training_data = load_data(df_directory)
+    dict_training_data = json.loads(training_data.as_json())
+    construct_domain(df_directory, dict_training_data)
+
+    # # Step -2
+    construct_interpreter(training_data)
+    #
+    # #step - 3
+    construct_stories(df_directory)
+
+
+construct_rasa_core("/home/bhavani/apps/mlandds/rasa_nlu/data/examples/WordBot")
---
+++ nlu_data.md
@@ -0,0 +1,43 @@
+## intent:Default Welcome Intent
+- whats up
+- heyyyy
+- yo
+- hello
+- hey
+- hi
+
+## intent:get_antonym
+- antonym of [loop](word)
+- opposite of [lame](word)
+- what is the opposite of [word](word)
+- what is the antonym of [word](word)
+
+## intent:get_antonym_context
+- what is its antonym
+- can you tell me its antonym
+- What is its opposite
+
+## intent:get_meaning
+- what is the meaning of the word [super](word)
+- what is the meaning of the word [dictionary](word)
+- what is the meaning of the word [system](word)
+- what is the meaning of the word [sweet](word)
+- what is the meaning of the word [spy](word)
+- what is the meaning of the word [dry](word)
+- tell me the meaning of [possible](word)
+- tell me the meaning of echo
+- tell me the meaning of [decode](word)
+- define [sleep](word)
+- define [sky](word)
+- define [nature](word)
+- what is the meaning of [dry](word)
+- what is the meaning of [elevate](word)
+- what is the meaning of [echo](word)
+
+## intent:get_meaning_context
+- can you tell its meaning
+- tell me it meaning
+- what is its meaning
+
+## synonym:durable
+- durable
